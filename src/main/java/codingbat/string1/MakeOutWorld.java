f73e25ae5819938667c36a7bf3a99e2bb948030d
Ermek <ermash.adiev@gmail.com>
2024-08-09 13:49:23 +0600

    econd commit to String1

---
+++ src/main/java/codingbat/string1/MakeOutWorld.java
@@ -0,0 +1,8 @@
+package codingbat.string1;
+
+public class MakeOutWorld {
+
+    public String makeOutWord(String out, String word) {
+        return out.substring(0, 2) + word + out.substring(2);
+    }
+}
